7efcc9667e9e49d40d0732a56649f32013b6c79f
Aryaman Jeendgar <jeendgararyaman@gmail.com>
2023-08-23 19:17:37 +0530

    feat: minor changes to `write` and `read` skeleton

--- src/uhi_serialization/__init__.py
+++ src/uhi_serialization/__init__.py
@@ -6,6 +6,7 @@ UHI-serialization: An implementation of the UHI serialization format
 
 
 from __future__ import annotations
+from .hdf5_serialization import write_hdf5_schema, read_hdf5_schema
 
 __version__ = "0.1.0"
 
--- src/uhi_serialization/hdf5_serialization.py
+++ src/uhi_serialization/hdf5_serialization.py
@@ -51,49 +51,35 @@ def write_hdf5_schema(file_name, histograms: dict[str, bh.Histogram]) -> h5py.Fi
             """Iterating through the axes, calling `create_axes_object` for each of them,
             creating references to new groups and appending it to the `items` dataset defined above"""
             current_axis = CONSTS['axes_dict'][str(axis)[: str(axis).index('(')]]
+            dataset = f[group_prefix + '/axes/items']
+            args_dict = {}
             match current_axis:
                 case 'regular':
-                    args_dict = {}
                     args_dict['bins'] = len(axis.edges) - 1
                     args_dict['lower'] = axis.edges[0]
                     args_dict['upper'] = axis.edges[-1]
                     args_dict['underflow'] = axis.traits.underflow
                     args_dict['overflow'] = axis.traits.overflow
                     args_dict['circular'] = axis.traits.circular
-                    if axis.metadata is not None:
-                        args_dict['metadata'] = axis.metadata
-                        create_axes_object(current_axis, f, name, i, True, args_dict)
-                    else:
-                        create_axes_object(current_axis, f, name, i, False, args_dict)
                 case 'variable':
-                    args_dict = {}
                     args_dict['edges'] = axis.edges
                     args_dict['underflow'] = axis.traits.underflow
                     args_dict['overflow'] = axis.traits.overflow
                     args_dict['circular'] = axis.traits.circular
-                    if axis.metadata is not None:
-                        args_dict['metadata'] = axis.metadata
-                        create_axes_object(current_axis, f, name, i, True, args_dict)
-                    else:
-                        create_axes_object(current_axis, f, name, i, False, args_dict)
                 case 'boolean':
-                    args_dict = {}
-                    if axis.metadata is not None:
-                        args_dict['metadata'] = axis.metadata
-                        create_axes_object(current_axis, f, name, i, True, args_dict)
-                    else:
-                        create_axes_object(current_axis, f, name, i, False, args_dict)
+                    #NOTE: Boolean axes may only have `metadata` as user-input options
+                    pass
                 case 'category_int' | 'category_str':
-                    args_dict = {}
                     s = str(axis)
                     args_dict['items'] = np.array(ast.literal_eval(s[s.find('['): s.find(']') + 1]),
                                                   dtype=object)
                     args_dict['flow'] = axis.traits.growth
-                    if axis.metadata is not None:
-                        args_dict['metadata'] = axis.metadata
-                        create_axes_object(current_axis, f, name, i, True, args_dict)
-                    else:
-                        create_axes_object(current_axis, f, name, i, False, args_dict)
+
+            if axis.metadata is not None:
+                args_dict['metadata'] = axis.metadata
+                dataset[i] = create_axes_object(current_axis, f, name, i, True, args_dict)[1]
+            else:
+                dataset[i] = create_axes_object(current_axis, f, name, i, False, args_dict)[1]
         """
         `axes` code end
         """
@@ -104,23 +90,22 @@ def write_hdf5_schema(file_name, histograms: dict[str, bh.Histogram]) -> h5py.Fi
         f[group_prefix].create_group('storage')
         f[group_prefix + '/storage'].attrs['description'] = "The storage of the bins of the histogram."
         hist_str_type = str(histogram.storage_type)
-        hist_str_type = hist_str_type[hist_str_type.find('e') + 2 : len(hist_str_type) - 2]
+        hist_str_type = CONSTS['storage_dict'][hist_str_type[hist_str_type.find('e') + 2 : len(hist_str_type) - 2]]
         args_dict = {}
-        args_dict['items'] = histogram.values()
+        args_dict['values'] = np.array(histogram.values())
         match hist_str_type:
             case 'int_storage':
-                args_dict['values'] = np.array(histogram.values())
+                #NOTE: `int_storage` only has the stored values
+                pass
             case 'double_storage':
-                args_dict['values'] = np.array(histogram.values())
+                #NOTE: `double_storage` only has the stored values
+                pass
             case 'weighted_storage':
-                args_dict['values'] = np.array(histogram.values())
                 args_dict['variances'] = np.array(histogram.variances())
             case 'mean_storage':
-                args_dict['values'] = np.array(histogram.values())
                 args_dict['variances'] = np.array(histogram.variances())
                 args_dict['count'] = np.array(histogram.counts())
             case 'weighted_mean_storage':
-                args_dict['values'] = np.array(histogram.values())
                 args_dict['variances'] = np.array(histogram.variances())
                 f1 = lambda x: x.sum_of_weights
                 f2 = lambda x: x.sum_of_weights_squared
@@ -130,16 +115,100 @@ def write_hdf5_schema(file_name, histograms: dict[str, bh.Histogram]) -> h5py.Fi
         """
         `storage` code end
         """
+    f.close()
     return f
 
 
+def read_hdf5_schema(input_file: h5py.File | Path) -> dict[str, bh.Histogram]:
+    f = input_file
+    if isinstance(input_file, Path):
+        f = h5py.File(f)
+    # The first level in the schema are the various histograms that have been serialized
+    for hist_name in list(f.keys()):
+        base_prefix = f"/{hist_name}"
+
+        #### `metadata` code start
+        metadata = {}
+        metadata_ref = f[base_prefix + '/metadata']
+        for (key, value) in metadata_ref.attrs.items():
+            metadata[key] = value
+        #### `metadata` code end
+
+        #### `axes` code start
+        axes = []
+        axes_ref = f[base_prefix + '/axes']
+        for i, unref_axis_ref in enumerate(axes_ref['items']):
+            deref_axis_ref = f[unref_axis_ref]
+            axis_type = deref_axis_ref.attrs['type']
+            args_dict = {}
+            #HACK: Force-adding the metadata field in `args_dict` allows me to avoid
+            # making if-else test around the existence of metadata for the current axis
+            args_dict['metadata'] = {}
+            for (key, value) in deref_axis_ref.attrs:
+                args_dict[key] = value
+            match axis_type:
+                case 'regular':
+                    axes.append(
+                        bh.axis.Regular(
+                            args_dict['bins'],
+                            args_dict['lower'],
+                            args_dict['upper'],
+                            overflow=args_dict['overflow'],
+                            underflow=args_dict['underflow'],
+                            circular=args_dict['circular'],
+                            metadata=args_dict['metadata']
+                        )
+                    )
+                case 'variable':
+                    args_dict['edges'] = np.array(deref_axis_ref[f'axis_{i}_edges'])
+                    axes.append(
+                        bh.axis.Variable(
+                            args_dict['edges'],
+                            underflow=args_dict['underflow'],
+                            overflow=args_dict['overflow'],
+                            circular=args_dict['circular'],
+                            metadata=args_dict['metadata']
+                        )
+                    )
+                case 'boolean':
+                    axes.append(
+                        bh.axis.Boolean(
+                            metadata=args_dict['metadata']
+                        )
+                    )
+                case 'category_int':
+                    args_dict['items'] = np.array(deref_axis_ref[f'axis_{i}_categories'])
+                    axes.append(
+                        bh.axis.IntCategory(
+                            args_dict['items'],
+                            growth=args_dict['flow'],
+                            metadata=args_dict['metadata']
+                        )
+                    )
+                case 'category_str':
+                    args_dict['items'] = np.array(deref_axis_ref[f'axis_{i}_categories'])
+                    axes.append(
+                        bh.axis.IntCategory(
+                            args_dict['items'],
+                            growth=args_dict['flow'],
+                            metadata=args_dict['metadata']
+                        )
+                    )
+        #### `axes` code end
+
+        #### `storage` code start
+
+        #### `storage` code end
+    return {
+        "": bh.Histogram()
+    }
+
 def create_axes_object(axis_type: str, hdf5_ptr: h5py.File, hist_name: str,
-                       axis_num: int, has_metadata: bool, args_dict, *args) -> h5py.File:
+                       axis_num: int, has_metadata: bool, args_dict, *args) -> tuple[h5py.File, h5py.Reference]:
     """Helper function for constructing and adding a new axis in the /ref_storage subfolder inside
     /hist_name of the hdf5_ptr file"""
     hist_folder_storage = hdf5_ptr['/{}/ref_storage'.format(hist_name)]
-    ref = hist_folder_storage.create_group(
-                    'axis_{}'.format(axis_num))
+    ref = hist_folder_storage.create_group(f'axis_{axis_num}')
     match axis_type:
         case "regular":
             ref.attrs['type'] = axis_type
@@ -197,18 +266,17 @@ def create_axes_object(axis_type: str, hdf5_ptr: h5py.File, hist_name: str,
                 ref.create_group('metadata')
                 for (key, value) in args_dict['metadata'].items():
                     ref['/metadata'].attrs[key] = value
-    return hdf5_ptr
+    return (hdf5_ptr, ref.ref)
 
 
 def create_storage_object(storage_type: str, hdf5_ptr: h5py.File, hist_name: str,
                           args_dict, *args) -> h5py.File:
     """Helper function for constructing and storing the main data in the /ref_storage
     subfolder inside /hist_name of the hdf5_ptr file"""
-    hist_folder_storage = hdf5_ptr['/{}/ref_storage'.format(hist_name)]
-    ref = hist_folder_storage.create_group('data')
+    ref = hdf5_ptr[f'/{hist_name}/storage']
     ref.attrs['type'] = storage_type
     ref.create_dataset('data', shape=args_dict['values'].shape, data=args_dict['values'])
-    storage_type = CONSTS['storage_dict'][storage_type]
+    # storage_type = CONSTS['storage_dict'][storage_type]
     match storage_type:
         case "int_storage":
             ref.attrs['description'] = "A storage holding integer counts."
@@ -229,5 +297,3 @@ def create_storage_object(storage_type: str, hdf5_ptr: h5py.File, hist_name: str
 
     return hdf5_ptr
 
-def read_hdf5_schema(input_file: h5py.File | Path) -> bh.Histogram:
-    return bh.Histogram()
